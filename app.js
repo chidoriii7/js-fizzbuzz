ae3125d17280e74350957ebab90781840f4e5764
chidoriii7 <muratore.danilo@yahoo.it>
2024-05-01 10:58:17 +0200

    js-fizzbuzz : BONUS 1

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
-// - ciclo for per stampare in console numeri da 1 a 100
-// - se il numero è divsibile per 3 stampare "fizz"
-// -(colore verde chiaro)
-// - se il numero è divisibile per 5 stampare "buzz"
-// (colore giallo)
-// - se il numero è divisibile sia per 3 che per 5 stampare "fizzbuzz"
-// (colore rosso)
 
+
+const containerDOM = document.querySelector('div.container');
+
+// - ciclo for per stampare in console numeri da 1 a 100
 for (let number = 1; number <= 100; number++ ){
     
+    //aggiungere una casella per ogni giro del ciclo for
+    const divElement  = document.createElement('div')
+    
+    
     if (number % 3 === 0 && number % 5 === 0) {
-        console.log('fizzbuzz')
+        
+        
+        containerDOM.append(divElement)
+        divElement.append(number)
+        // (colore rosso)
+        divElement.classList.add('box')
+        console.log('FizzBuzz');
+
+
+        // - se il numero è divsibile per 3 stampare "fizz"
     } else if (number % 3 === 0) {
-        console.log('fizz')
+        
+    
+        containerDOM.append(divElement)
+        divElement.append('Fizz')
+        // -(colore verde chiaro)
+        divElement.classList.add('box')
+        console.log('fizz');
+        
+        
+        // - se il numero è divisibile per 5 stampare "buzz"
     } else if (number % 5 === 0) {
-        console.log('buzz')
+        
+        containerDOM.append(divElement)
+        divElement.append('Buzz')
+
+
+        // (colore giallo)
+        divElement.classList.add('box')
+
+        console.log('buzz');
+
     } else {
-        console.log(number)
+        containerDOM.append(divElement)
+        divElement.append(number)
+
+        //colore azzurro
+        divElement.classList.add('box')
+        console.log(number);
+
     }
 
 }
